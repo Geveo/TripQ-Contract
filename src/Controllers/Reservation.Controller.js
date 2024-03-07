b36d47ce600e308e0428f6bfb5727a497a551250
UdithIndrakantha <udith.indrakantha@gmail.com>
2024-03-07 22:47:30 +0530

    Added a method to get reservations

---
+++ src/Controllers/Reservation.Controller.js
@@ -0,0 +1,27 @@
+import { ReservationService } from "../Services/Domain.Services/ReservationService";
+import { ActivityLogService } from "../Services/Common.Services/ActivityLog.Service";
+
+export class ReservationController {
+	#message = null;
+	#service = null;
+	#activityLogger = null;
+
+	constructor(message) {
+		this.#message = message;
+		this.#service = new ReservationService(message);
+	}
+
+	async handleRequest() {
+		try {
+			switch (this.#message.subType) {
+				case "GetReservations":
+					return await this.#service.getReservations();
+					break;
+				default:
+					break;
+			}
+		} catch (error) {
+			return { error: error };
+		}
+	}
+}
--- src/controller.js
+++ src/controller.js
@@ -4,6 +4,7 @@ import { AuthService } from "./Services/Common.Services/Auth.service";
 import { MessageAuthenticationService } from "./Services/Common.Services/MessageAuthenticationService";
 import { HotelController } from "./Controllers/Hotel.Controller";
 import { RoomController } from "./Controllers/Room.Controller";
+import { ReservationController } from "./Controllers/Reservation.Controller";
 
 const settings = require("./settings.json").settings;
 
@@ -13,11 +14,13 @@ export class Controller {
 	#hotelController = null;
 	#roomController = null;
 	#messageAuthService = null;
+	#reservationController = null;
 
 	async handleRequest(user, message, isReadOnly) {
 		this.#contractController = new ContractUpdateController(message);
 		this.#hotelController = new HotelController(message);
 		this.#roomController = new RoomController(message);
+		this.#reservationController = new ReservationController(message);
 		//this.#messageAuthService = new MessageAuthenticationService();
 
 		//let _authService = new AuthService();
@@ -57,6 +60,9 @@ export class Controller {
 		}
 		if (message.type == ServiceTypes.ROOM) {
 			result = await this.#roomController.handleRequest();
+		}
+		if(message.type == ServiceTypes.RESERVATION) {
+			result = await this.#reservationController.handleRequest();
 		}
 
 		if (isReadOnly) {
